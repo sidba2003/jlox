b2d2fb458f155846db0361a8f24549924abb7572
siddharthbahl <bahlsiddharth123@gmail.com>
2024-06-17 03:06:19 +0100

    Adding parsig capabilities for single operators

---
+++ src/main/java/com/interpreter/jlox/Scanner.java
@@ -0,0 +1,68 @@
+package src.main.java.com.interpreter.jlox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static src.main.java.com.interpreter.jlox.TokenType.*;
+
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source){
+        this.source = source;
+    }
+
+    public List<Token> scanTokens(){
+        while (!isAtEnd()){
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+          case '(': addToken(LEFT_PAREN); break;
+          case ')': addToken(RIGHT_PAREN); break;
+          case '{': addToken(LEFT_BRACE); break;
+          case '}': addToken(RIGHT_BRACE); break;
+          case ',': addToken(COMMA); break;
+          case '.': addToken(DOT); break;
+          case '-': addToken(MINUS); break;
+          case '+': addToken(PLUS); break;
+          case ';': addToken(SEMICOLON); break;
+          case '*': addToken(STAR); break;
+          default:
+            Lox.error(line, "unexpected character");
+            break;
+        }
+    }
+
+    private char advance(){
+        return source.charAt(current++);
+    }    
+
+    private void addToken(TokenType type){
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean isAtEnd(){
+        return current >= source.length();
+    }
+}
